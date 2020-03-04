fc1ebe39336a16ad8c3719660f725a209e876dd2
Eduardo Ramos <luis.ramos@metasix.com.br>
2020-03-04 12:04:56 -0300

    serviço de autenticação e reset de formulario

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
-  templateUrl: "./signin.component.html"
+	templateUrl: "./signin.component.html"
 })
 export class SignInComponent implements OnInit {
-  loginForm: FormGroup;
+	loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+	constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
 
-  ngOnInit(): void {
-    this.loginForm = this.formBuilder.group({
-      userName: ["", Validators.required],
-      password: ["", Validators.required]
-    });
-  }
+	ngOnInit(): void {
+		this.loginForm = this.formBuilder.group({
+			userName: ["", Validators.required],
+			password: ["", Validators.required]
+		});
+	}
+
+	login() {
+		const userName = this.loginForm.get('userName').value
+		const password = this.loginForm.get('password').value
+
+		this.authService.authenticate(userName, password).subscribe(
+			() => { console.log("autenticado") },
+			(err) => {
+				console.log(err)
+				alert("Nome de usuário ou senha inválidos")
+				// reseta todos os valores do formulario
+				this.loginForm.reset()
+			}
+		)
+	}
 }
