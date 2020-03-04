95119b6d85a101d07587c2cbc0dc7d9b662b65e4
Eduardo Ramos <luis.ramos@metasix.com.br>
2020-03-04 14:46:07 -0300

    Armazenamento do token

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-
 // permite ser usado em um pipe para pegar algum tipo de informação da requisiçao
 import { tap } from "rxjs/operators";
+
+import { TokenService } from "../token/token.service";
 
 const API_URL = "http://localhost:3000";
 
@@ -11,7 +12,7 @@ const API_URL = "http://localhost:3000";
   providedIn: "root"
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
   authenticate(userName: string, password: string) {
     return this.http
@@ -27,6 +28,7 @@ export class AuthService {
       .pipe(
         tap(res => {
           const authToken = res.headers.get("x-access-token");
+          this.tokenService.setToken(authToken);
           console.log(authToken);
         })
       );
