49e18b5bc4613cce692b9325fa4f5a24f82662df
Eduardo Ramos <luis.ramos@metasix.com.br>
2020-03-04 14:19:39 -0300

    Extraindo token da resposta da requisição

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,21 +1,34 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
-const API_URL = 'http://localhost:3000'
+// permite ser usado em um pipe para pegar algum tipo de informação da requisiçao
+import { tap } from "rxjs/operators";
+
+const API_URL = "http://localhost:3000";
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-	constructor(private http: HttpClient) { }
-
-	authenticate(userName: string, password: string) {
-		return this.http.post(`${API_URL}/user/login`,
-			{
-				userName,
-				password
-			})
-	}
+  authenticate(userName: string, password: string) {
+    return this.http
+      .post(
+        `${API_URL}/user/login`,
+        {
+          userName,
+          password
+        },
+        // permite que eu tenha acesso as informações da resposta
+        { observe: "response" }
+      )
+      .pipe(
+        tap(res => {
+          const authToken = res.headers.get("x-access-token");
+          console.log(authToken);
+        })
+      );
+  }
 }
