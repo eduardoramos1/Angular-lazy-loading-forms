981ef231ca56c65a5ebc0d25e0a16753826ad2f9
Eduardo Ramos <luis.ramos@metasix.com.br>
2020-03-06 18:33:12 -0300

    Upload de imagens

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { PhotoService } from "../photo/photo.service";
+import { Router } from "@angular/router";
 
 @Component({
 	selector: "ap-photo-form",
@@ -9,7 +11,11 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 export class PhotoFormComponent implements OnInit {
 	photoForm: FormGroup;
 	file: File;
-	constructor(private formBuilder: FormBuilder) {}
+	constructor(
+		private formBuilder: FormBuilder,
+		private photoService: PhotoService,
+		private router: Router
+	) {}
 
 	ngOnInit() {
 		this.photoForm = this.formBuilder.group({
@@ -22,7 +28,9 @@ export class PhotoFormComponent implements OnInit {
 	upload() {
 		const description = this.photoForm.get("description").value;
 		const allowComments = this.photoForm.get("allowComments").value;
-		console.log(description, allowComments);
-		console.log(this.file);
+
+		this.photoService
+			.upload(description, allowComments, this.file)
+			.subscribe(() => this.router.navigate([""]));
 	}
 }
--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,12 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
+
+const cloud = "http://localhost:3000/imgs/";
 
 @Component({
-    selector: 'ap-photo',
-    templateUrl: 'photo.component.html'
+	selector: "ap-photo",
+	templateUrl: "photo.component.html"
 })
 export class PhotoComponent {
-    
-    @Input() description='';
-    
-    @Input() url='';
+	private _url = "";
+
+	@Input() description = "";
+
+	// Em angular eu posso aplicar uma inbounding property para um set, e então aplicar algum tipo de lógica nele.
+	@Input() set url(url: string) {
+		// se a url começar com "data", _url recebe concatenação de cloud + url
+		if (!url.startsWith("data")) {
+			this._url = cloud + url;
+		} else {
+			this._url = url;
+		}
+	}
+
+	get url() {
+		return this._url;
+	}
 }
