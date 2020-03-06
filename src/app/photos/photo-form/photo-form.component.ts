5a44d2515277ea16f9c6dad6601d11457f775e11
Eduardo Ramos <luis.ramos@metasix.com.br>
2020-03-06 17:21:44 -0300

    Formulário photo-form e validações/ reactiveForm

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'ap-photo-form',
-  templateUrl: './photo-form.component.html',
-  styleUrls: ['./photo-form.component.css']
+	selector: "ap-photo-form",
+	templateUrl: "./photo-form.component.html",
+	styleUrls: ["./photo-form.component.css"]
 })
 export class PhotoFormComponent implements OnInit {
+	photoForm: FormGroup;
+	constructor(private formBuilder: FormBuilder) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+		this.photoForm = this.formBuilder.group({
+			file: ["", Validators.required],
+			description: ["", Validators.maxLength(300)],
+			allowComments: [true]
+		});
+	}
 }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -4,24 +4,29 @@ import { CommonModule } from "@angular/common";
 
 import { PhotoComponent } from "./photo/photo.component";
 import { PhotoListComponent } from "./photo-list/photo-list.component";
-import { PhotoFormComponent } from "./photo-form/photo-form.component";
 import { PhotosComponent } from "./photo-list/photos/photos.component";
 import { FilterByDescription } from "./photo-list/filter-by-description.pipe";
 import { LoadButtonComponent } from "./photo-list/load-button/load-button.component";
 import { CardModule } from "../shared/components/card/card.module";
 import { SearchComponent } from "./photo-list/search/search.component";
 import { DarkenOnHoverModule } from "../shared/directives/dark-on-hover/dark-on-hover.module";
+import { PhotoFormModule } from "./photo-form/photo-form.module";
 
 @NgModule({
-  declarations: [
-    PhotoComponent,
-    PhotoListComponent,
-    PhotoFormComponent,
-    PhotosComponent,
-    FilterByDescription,
-    LoadButtonComponent,
-    SearchComponent
-  ],
-  imports: [HttpClientModule, CommonModule, CardModule, DarkenOnHoverModule]
+	declarations: [
+		PhotoComponent,
+		PhotoListComponent,
+		PhotosComponent,
+		FilterByDescription,
+		LoadButtonComponent,
+		SearchComponent
+	],
+	imports: [
+		HttpClientModule,
+		CommonModule,
+		CardModule,
+		DarkenOnHoverModule,
+		PhotoFormModule
+	]
 })
 export class PhotosModule {}
