a45581a0fce9ae222b39a75446ab5e5c149b1363
Eduardo Ramos <luis.ramos@metasix.com.br>
2020-03-09 16:00:13 -0300

    diretiva para clique automático

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -5,6 +5,7 @@ import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { VMessageModule } from "src/app/shared/components/vmessage/vmessage.module";
 import { RouterModule } from "@angular/router";
 import { PhotoModule } from "../photo/photo.module";
+import { ImmediateClickModule } from "src/app/shared/directives/immediate-click/immediate-click.module";
 
 @NgModule({
 	declarations: [PhotoFormComponent],
@@ -13,7 +14,8 @@ import { PhotoModule } from "../photo/photo.module";
 		ReactiveFormsModule,
 		VMessageModule,
 		RouterModule,
-		PhotoModule
+		PhotoModule,
+		ImmediateClickModule
 	]
 })
 export class PhotoFormModule {}
