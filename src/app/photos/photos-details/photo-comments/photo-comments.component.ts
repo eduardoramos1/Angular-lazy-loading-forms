4813adc1ba8690735d94845dbe486dc5f56f9894
Eduardo Ramos <luis.ramos@metasix.com.br>
2020-03-12 16:39:45 -0300

    Validação do form de comentários e incluindo estilo global

--- src/app/photos/photos-details/photo-comments/photo-comments.component.ts
+++ src/app/photos/photos-details/photo-comments/photo-comments.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { PhotoComment } from '../../photo/photo-comment';
 import { Observable } from 'rxjs';
 import { PhotoService } from '../../photo/photo.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
 	templateUrl: './photo-comments.component.html',
@@ -9,12 +10,16 @@ import { PhotoService } from '../../photo/photo.service';
 })
 export class PhotoCommentsComponent implements OnInit {
 	@Input() photoId: number;
+	commentForm: FormGroup;
 
 	comments$: Observable<PhotoComment[]>;
 
-	constructor(private photoService: PhotoService) {}
+	constructor(private photoService: PhotoService, private formBuilder: FormBuilder) {}
 
 	ngOnInit() {
 		this.comments$ = this.photoService.getComments(this.photoId);
+		this.commentForm = this.formBuilder.group({
+			comment: [ '', Validators.maxLength(300) ]
+		});
 	}
 }
--- src/app/photos/photos-details/photo-details.component.ts
+++ src/app/photos/photos-details/photo-details.component.ts
@@ -3,11 +3,9 @@ import { ActivatedRoute } from '@angular/router';
 import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
 import { Observable } from 'rxjs';
-import { PhotoComment } from '../photo/photo-comment';
 
 @Component({
-	templateUrl: './photo-details.component.html',
-	styleUrls: [ './photo-details.css' ]
+	templateUrl: './photo-details.component.html'
 })
 export class PhotoDetailsComponent implements OnInit {
 	photo$: Observable<Photo>;
